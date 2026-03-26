0c86d40e1ef95320b56882f16d485639d07d7aeb
sumalatha <sumagaddipati@gmail.com>
2026-03-26 10:56:11 +0530

    api integration

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://careconnect-backend-1-603l.onrender.com/api",
 });
 
 api.interceptors.request.use(config => {
@@ -13,4 +13,3 @@ api.interceptors.request.use(config => {
 });
 
 export default api;
-
